75fc3c3de712c464d131e1b0526a2e66b5933d0c
ilke <ilke.07.kartal@gmail.com>
2022-10-13 20:10:52 +0300

    Proje dosyası ekle.

---
+++ proje5/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace proje5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbHarf = new System.Windows.Forms.ComboBox();
+            this.btnKontrol = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbHarf
+            // 
+            this.cmbHarf.FormattingEnabled = true;
+            this.cmbHarf.Items.AddRange(new object[] {
+            "A",
+            "M",
+            "B",
+            "C",
+            "Ç",
+            "D",
+            "E",
+            "U",
+            "F",
+            "G",
+            "Ğ",
+            "H",
+            "I",
+            "İ",
+            "J",
+            "K",
+            "L",
+            "N",
+            "O",
+            "Ö",
+            "Z",
+            "P",
+            "R",
+            "S",
+            "Ş",
+            "T",
+            "Ü",
+            "V",
+            "Y"});
+            this.cmbHarf.Location = new System.Drawing.Point(300, 131);
+            this.cmbHarf.Name = "cmbHarf";
+            this.cmbHarf.Size = new System.Drawing.Size(121, 24);
+            this.cmbHarf.TabIndex = 0;
+            // 
+            // btnKontrol
+            // 
+            this.btnKontrol.Location = new System.Drawing.Point(311, 225);
+            this.btnKontrol.Name = "btnKontrol";
+            this.btnKontrol.Size = new System.Drawing.Size(75, 23);
+            this.btnKontrol.TabIndex = 1;
+            this.btnKontrol.Text = "kontrol et";
+            this.btnKontrol.UseVisualStyleBackColor = true;
+            this.btnKontrol.Click += new System.EventHandler(this.btnKontrol_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKontrol);
+            this.Controls.Add(this.cmbHarf);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbHarf;
+        private System.Windows.Forms.Button btnKontrol;
+    }
+}
+
---
+++ proje5/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKontrol_Click(object sender, EventArgs e)
+        {
+            string harf = cmbHarf.SelectedItem.ToString();
+            MessageBox.Show($"Seçilen Harf: {harf}, Harf Sırası: {Alfabe_Sirasi(harf)}");
+        }
+        private int Alfabe_Sirasi (string harf)
+        {
+            switch(harf[0])
+            { 
+            case 'a': case 'A': return 1;
+            case 'b': case 'B': return 2;
+            case 'c': case 'C': return 3;
+            case 'ç': case 'Ç': return 4;
+            case 'd': case 'D': return 5;
+            case 'e': case 'E': return 6;
+            case 'f': case 'F': return 7;
+            case 'g': case 'G': return 8;
+            case 'ğ': case 'Ğ': return 9;
+            case 'h': case 'H': return 10;
+            case 'ı': case 'I': return 11;
+            case 'i': case 'İ': return 12;
+            case 'j': case 'J': return 13;
+            case 'k': case 'K': return 14;
+            case 'l': case 'L': return 15;
+            case 'm': case 'M': return 16;
+            case 'n': case 'N': return 17;
+            case 'o': case 'O': return 18;
+            case 'ö': case 'Ö': return 19;
+            case 'p': case 'P': return 20;
+            case 'r': case 'R': return 21;
+            case 's': case 'S': return 22;
+            case 'ş': case 'Ş': return 23;
+            case 't': case 'T': return 24;
+            case 'u': case 'U': return 25;
+            case 'ü': case 'Ü': return 26;
+            case 'v': case 'V': return 27;
+            case 'y': case 'Y': return 28;
+            case 'z': case 'Z': return 29;
+            }
+            return 0;
+        }
+    }
+} 
